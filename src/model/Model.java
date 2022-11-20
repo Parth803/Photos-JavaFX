8f312c8a9ae365cd41c99aea685a0bcb9591c5c1
Parth Patel <parthp1217@gmail.com>
2022-11-20 12:11:31 -0500

    needs fixing

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,7 +1,10 @@
 package model;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 public final class Model {
     public static ArrayList<User> users;
@@ -10,17 +13,25 @@ public final class Model {
     }
 
     public static void initializeModel() {
-        users = new ArrayList<>();
         File userFile = new File("data/admin/users.txt");
+        if (userFile.length() == 0) {
+            users = new ArrayList<>();
+        }
+        else {
+            try (Stream<String> stream = Files.lines(Paths.get("data/admin/users.txt"))) {
+                stream.forEach(line -> users.add(new User(line)));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
         // else ->
         // users = readSerializedArrayListOfTypeUser();
-        if (userFile.length() != 0) {
             try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("data/admin/users.txt"))) {
                 users = (ArrayList<User>) in.readObject();
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        }
     }
     // use java.io.ObjectOutputStream/java.io.ObjectInputStream classes to:
     // create methods to store and retrieve the ArrayList of users in data/admin/users.txt
