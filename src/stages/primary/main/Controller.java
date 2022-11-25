05e3d1499abddb323ff3880d7f557f23ba1aaf0e
Parth Patel <parthp1217@gmail.com>
2022-11-25 17:51:25 -0500

    fixed login controller + removed menubar for a toolbar

--- src/stages/primary/main/Controller.java
+++ src/stages/primary/main/Controller.java
@@ -1,15 +1,11 @@
 package stages.primary.main;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
 import model.Model;
-import model.Photo;
 import photos.Photos;
 
 public class Controller {
@@ -20,30 +16,36 @@ public class Controller {
     @FXML
     private Text warning;
     public void initialize() {
-        this.proceed.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                continueAction();
-            }
-        });
+        this.proceed.setOnAction(actionEvent -> continueAction());
     }
 
     public void continueAction() {
-        Model.initializeModel();
         try {
             Model.setCurrentUser(username.getText());
             warning.setOpacity(0);
-            Stage primaryStage = Photos.getPrimaryStage();
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/albums/albums.fxml"));
-            Parent root = null;
-            try {
-                root = loader.load();
-            } catch (Exception e) {
-                e.printStackTrace();
+
+            if (username.getText().equals("admin")) {
+                java.net.URL obj = Photos.class.getResource("/stages/primary/admin/admin.fxml");
+                if (obj == null) {
+                    // handle this in GUI with alert dialog
+                    System.out.println("FXML not found");
+                    throw new NullPointerException();
+                }
+                Parent root = FXMLLoader.load(obj);
+                Scene scene = new Scene(root);
+                Photos.getPrimaryStage().setScene(scene);
             }
-            Controller controller = loader.getController();
-            Scene scene = new Scene(root);
-            primaryStage.setScene(scene);
+            else {
+                java.net.URL obj = Photos.class.getResource("/stages/primary/albums/albums.fxml");
+                if (obj == null) {
+                    // handle this in GUI with alert dialog
+                    System.out.println("FXML not found");
+                    throw new NullPointerException();
+                }
+                Parent root = FXMLLoader.load(obj);
+                Scene scene = new Scene(root);
+                Photos.getPrimaryStage().setScene(scene);
+            }
         } catch (Exception e) {
             warning.setOpacity(0.69);
         }
