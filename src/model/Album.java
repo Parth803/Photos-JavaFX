69fa970e91a92f7e5f1e164b0cd7519a475c1619
YashPatel709 <yash.p.patel@rutgers.edu>
2022-11-25 12:14:56 -0500

    got rid of unnecessary imports

--- src/model/Album.java
+++ src/model/Album.java
@@ -3,7 +3,6 @@ package model;
 import java.io.Serial;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Comparator;
 
 public final class Album implements java.io.Serializable {
     @Serial
