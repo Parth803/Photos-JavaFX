75435ecd71f67bf7cffa3c367306e79ff9bc592d
Parth Patel <parthp1217@gmail.com>
2022-11-26 07:20:10 -0500

    added back and logout function so i dont have to do it everytime ;D

--- src/stages/primary/albums/Controller.java
+++ src/stages/primary/albums/Controller.java
@@ -1,4 +1,17 @@
 package stages.primary.albums;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import photos.Photos;
+
 public class Controller {
+    @FXML
+    private Button back;
+    @FXML
+    private Button logout;
+
+    public void initialize() {
+        this.back.setOnAction(actionEvent -> Photos.logOut());
+        this.logout.setOnAction(actionEvent -> Photos.logOut());
+    }
 }
--- src/stages/primary/search/Controller.java
+++ src/stages/primary/search/Controller.java
@@ -1,4 +1,21 @@
 package stages.primary.search;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import photos.Photos;
+
 public class Controller {
+    @FXML
+    private Button back;
+    @FXML
+    private Button logout;
+
+    public void initialize() {
+        this.back.setOnAction(actionEvent -> goBack());
+        this.logout.setOnAction(actionEvent -> Photos.logOut());
+    }
+
+    public void goBack() {
+
+    }
 }
