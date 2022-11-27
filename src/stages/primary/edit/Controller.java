384394e064a87bac9e82ae29f9fa68e871ed0aee
Parth Patel <parthp1217@gmail.com>
2022-11-27 09:21:36 -0500

    stuff

--- src/stages/primary/edit/Controller.java
+++ src/stages/primary/edit/Controller.java
@@ -1,21 +1,60 @@
 package stages.primary.edit;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import model.Model;
+import model.Photo;
 import photos.Photos;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Controller {
     @FXML
     private Button back;
     @FXML
     private Button logout;
-
+    @FXML
+    private TextField caption;
+    @FXML
+    private Button updateCaption;
+    @FXML
+    private Button deleteTag;
+    @FXML
+    private ListView<String> tagsList;
     public void initialize() {
+        Photo selectedPhoto = (Photo) Model.dataTransfer.get(1);
+        Model.dataTransfer.clear();
+
         this.back.setOnAction(actionEvent -> Photos.changeScene("primary", "/stages/primary/photoslist/photoslist.fxml"));
         this.logout.setOnAction(actionEvent -> Photos.changeScene("primary", "/stages/primary/main/main.fxml"));
+        this.deleteTag.setOnAction(actionEvent -> deleteTag(selectedPhoto));
+        this.updateCaption.setOnAction(actionEvent -> updateCaption());
+        this.tagsList.setItems(FXCollections.observableList(selectedPhoto.tags.stream().map(t -> t.type+"="+t.value).collect(Collectors.toList())));
+
     }
 
-    public void goBack() {
+    public void deleteTag(Photo selectedPhoto) {
+        if (this.tagsList.getSelectionModel().isEmpty()) {
+            return;
+        }
+        Pattern p = Pattern.compile("(\\S*)=(\\S*)");
+        Matcher m = p.matcher(this.tagsList.getSelectionModel().getSelectedItem());
+        if (m.find()) {
+            try {
+                Model.currentUser.uniquePhotos.get(selectedPhoto.path).removeTag(m.group(1), m.group(2));
+                this.tagsList.setItems(FXCollections.observableList(selectedPhoto.tags.stream().map(t -> t.type+"="+t.value).collect(Collectors.toList())));
+            } catch (Exception e) {
+                throw new RuntimeException("Error removing tag from photo");
+            }
+        }
+    }
+
+    public void updateCaption() {
 
     }
 }
